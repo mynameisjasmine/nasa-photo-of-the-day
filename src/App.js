dc1dd78d80017db3b858cfaefb94474583f9be76
Jasmine <jasminecharles@hotmail.com>
2019-07-17 13:53:47 -0700

    ajax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+    <Header />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Header() {
+return(
+    <div className="header">
+        <p className="header-text">SPACE PHOTO OF THE DAY</p>
+    </div>
+)
+}
+
+export default Header;
